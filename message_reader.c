7f9511c9f4661f02b5c18a058176b48b59bf4f5d
SegevBar <segevbar94@gmail.com>
2022-12-27 00:40:29 +0200

    changed message reader

--- message_reader.c
+++ message_reader.c
@@ -7,12 +7,12 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-    const char* file_path;
-    unsigned int channel_id;
+    const char* file_path;  /*message slot file path*/
+    unsigned int channel_id;  /*target message channel id*/
     char message[BUF_LEN];
     int fd;
     char* p = NULL; 
-    int length = 0;
+    int len = 0;
 
     if (argc != 3) {
         perror("Invalid number of arguments");
@@ -22,29 +22,34 @@ int main(int argc, char const *argv[]) {
     /* get CMD arguments */
     file_path = argv[1];
     channel_id = strtol(argv[2], &p, 10);
-
+    
+    /* Open the specified message slot device file */
     fd = open(file_path, O_RDWR);
     if (fd < 0) {
         perror("An error occurred while opening the file");
         exit(1);
     }
+    /* Set the channel id to the id specified on the command line */
     if (ioctl(fd, MSG_SLOT_CHANNEL, channel_id) < -1) {
         perror("An error occurred in ioctl function");
         exit(1);
     }
-    length = read(fd, message, BUF_LEN);
-    if (length < 0) {
+    /* Read a message from the message slot file to a buffer */
+    if ((len = read(fd, message, BUF_LEN)) < 0) {
         perror("An error occurred while reading the message");
         exit(1);
     }
+    /* Close the device */
     if (close(fd) < -1) {
         perror("An error occurred while closing file discriptor");
         exit(1);
     }
-    if (write(1, message, length) < 0) {
+    /* Print the message to standard output */
+    if (write(1, message, len) < 0) {
         perror("An error occurred while writing the message");
         exit(1);
     }
+    /* Exit the program with exit value 0 */
     exit(0);
     return 0;
 }
--- message_sender.c
+++ message_sender.c
@@ -8,12 +8,12 @@
 #include <string.h>
 
 int main(int argc, char const *argv[]) {
-    const char* file_path;
-    unsigned int channel_id;
-    const char* message;
+    const char* file_path;  /*message slot file path*/
+    unsigned int channel_id;  /*target message channel id*/
+    const char* message;  /*the message to pass*/
     int fd;
     char* p = NULL; 
-    int length = 0;
+    int len = 0;
 
     if (argc != 4) {
         perror("Invalid number of arguments");
@@ -22,27 +22,32 @@ int main(int argc, char const *argv[]) {
     
     /* get CMD arguments */
     file_path = argv[1];
-    channel_id = strtol(argv[2], &p,10);
+    channel_id = strtol(argv[2], &p, 10);
     message = argv[3];
-    length = strlen(message);  
+    len = strlen(message);  
 
+    /* Open the specified message slot device file */
     fd = open(file_path, O_RDWR);
     if (fd < 0) {
         perror("An error occurred while opening the file");
         exit(1);
     }
+    /* Set the channel id to the id specified on the command line */
     if (ioctl(fd, MSG_SLOT_CHANNEL, channel_id) < -1) {
         perror("An error occurred in ioctl function");
         exit(1);
     }
-    if (write(fd, message, length) < 0) {
+    /* Write the specified message to the message slot file */
+    if (write(fd, message, len) < 0) {
         perror("An error occurred while writing the message");
         exit(1);
     }
+    /* Close the device */
     if (close(fd) < -1) {
         perror("An error occurred while closing file discriptor");
         exit(1);
     }
+    /* Exit the program with exit value 0 */
     exit(0);
     return 0;
 }
